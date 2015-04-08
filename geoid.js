401e6e93e0385e2af59114eafd368bba6b1cbe7d
Logan Franken <loganfranken@gmail.com>
2015-04-07 20:19:53 -0700

    Wrapped definition in module pattern

--- geoid.js
+++ geoid.js
@@ -1,80 +1,85 @@
-var geoid = {};
-
-geoid.getPosition = function() {
-
-  // Settings
-  var minAccuracy = 100; // Measured in meters
-  var enableHighAccuracy = true;
-  var timeout = 30000; // 5 minutes
-  var maximumAge = 0; // Whether or not a cached position can be used
+var geoid = (function() {
 
   var watchId = null;
   var hasPosition = false;
   var currPosition = null;
 
-  // Immediately get an inaccurate current position as a fallback in case
-  // our other attempts fail
-  navigator.geolocation.getCurrentPosition(
+  function getPosition() {
 
-    // On Success
-    function(position) {
+    // Settings
+    var minAccuracy = 100; // Measured in meters
+    var enableHighAccuracy = true;
+    var timeout = 30000; // 5 minutes
+    var maximumAge = 0; // Whether or not a cached position can be used
 
-      // Don't override an accurate position (if it somehow came in first)
-      if(!geoid.hasPosition)
-      {
-        hasPosition = true;
-        currentPosition = position;
-      }
-
-    },
-
-    // On Error
-    function(error) {
-
-      // Swallow errors occurring from this initial call
-
-    },
-
-    // Settings (for a quick, inaccurate call)
-    {
-      enableHighAccuracy: false,
-      maximumAge: Math.Infinity
-    }
-
-  );
-
-  // Set up the promise for the more accurate geolocation call using
-  // repeated watchPosition calls
-  return new Promise(function(resolve, reject) {
-
-    watchId = navigator.geolocation.watchPosition(
+    // Immediately get an inaccurate current position as a fallback in case
+    // our other attempts fail
+    navigator.geolocation.getCurrentPosition(
 
       // On Success
       function(position) {
 
-        if(position.coords.accuracy <= minAccuracy) {
+        // Don't override an accurate position (if it somehow came in first)
+        if(!geoid.hasPosition)
+          {
+            hasPosition = true;
+            currentPosition = position;
+          }
 
-          // As soon as we have achieved the desired accuracy, clear the watch
-          navigator.geolocation.clearWatch(watchId);
-          resolve(position);
+        },
 
+        // On Error
+        function(error) {
+
+          // Swallow errors occurring from this initial call
+
+        },
+
+        // Settings (for a quick, inaccurate call)
+        {
+          enableHighAccuracy: false,
+          maximumAge: Math.Infinity
         }
 
-      },
+      );
 
-      // On Error
-      function(error) {
-        reject(error)
-      },
+      // Set up the promise for the more accurate geolocation call using
+      // repeated watchPosition calls
+      return new Promise(function(resolve, reject) {
 
-      // Settings
-      {
-        enableHighAccuracy: enableHighAccuracy,
-        timeout: timeout,
-        maximumAge: maximumAge
-      }
+        watchId = navigator.geolocation.watchPosition(
 
-    );
+          // On Success
+          function(position) {
 
-  });
-}
+            if(position.coords.accuracy <= minAccuracy) {
+
+              // As soon as we have achieved the desired accuracy, clear the watch
+              navigator.geolocation.clearWatch(watchId);
+              resolve(position);
+
+            }
+
+          },
+
+          // On Error
+          function(error) {
+            reject(error)
+          },
+
+          // Settings
+          {
+            enableHighAccuracy: enableHighAccuracy,
+            timeout: timeout,
+            maximumAge: maximumAge
+          }
+
+        );
+    });
+  }
+
+  return {
+    getPosition: getPosition,
+  };
+
+})();
