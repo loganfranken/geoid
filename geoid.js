882078883b251525eca10e6f48c88cb60ff94372
Logan Franken <loganfranken@gmail.com>
2015-04-10 19:55:17 -0700

    Moved timer logic into a separate function

--- geoid.js
+++ geoid.js
@@ -4,82 +4,75 @@ var geoid = (function() {
   var hasPosition = false;
   var currentPosition = null;
 
+  // Default Settings
+
+  // Minimimum Accuracy: The minimum level of accuracy (measured in meters)
+  // to accept
+  // Default: 10 meters (30 feet)
+  var minAccuracy = 10; // Default: 10 meters (30 feet)
+
+  // Enable High Accuracy: Whether or not an accurate, but possibly
+  // expensive, geolocation result should be returned
+  // Default: true
+  var enableHighAccuracy = true;
+
+  // Timeout: How long to wait for a result before giving up
+  // Default: 30 seconds
+  var timeout = 30000;
+
+  // Maximum Age: How old (in milliseconds) of a cached position to accept
+  // Default: 0 (don't accept a cached position)
+  var maximumAge = 0;
+
+  // Time Measure Interval: How often the timer (used in measuring the
+  // timeout is updated)
+  // Default: 10 milliseconds
+  var timeMeasureInterval = 10;
+
   function getPosition() {
-
-    // Default Settings
-
-    // Minimimum Accuracy: The minimum level of accuracy (measured in meters)
-    // to accept
-    // Default: 10 meters (30 feet)
-    var minAccuracy = 10; // Default: 10 meters (30 feet)
-
-    // Enable High Accuracy: Whether or not an accurate, but possibly
-    // expensive, geolocation result should be returned
-    // Default: true
-    var enableHighAccuracy = true;
-
-    // Timeout: How long to wait for a result before giving up
-    // Default: 30 seconds
-    var timeout = 30000;
-
-    // Maximum Age: How old (in milliseconds) of a cached position to accept
-    // Default: 0 (don't accept a cached position)
-    var maximumAge = 0;
 
     // Immediately get an inaccurate current position as a fallback in case
     // our other attempts fail
     getQuickPosition().then(function() {
 
-        // Don't override an accurate position (if it somehow came in first)
-        if(!geoid.hasPosition)
-        {
-          hasPosition = true;
-          currentPosition = position;
+      // Don't override an accurate position (if it somehow came in first)
+      if(!geoid.hasPosition)
+      {
+        hasPosition = true;
+        currentPosition = position;
+      }
+
+    }).catch(function() {
+
+      // Swallow the exception for this quick call
+
+    });
+
+    // Set up the promise for the more accurate geolocation call using
+    // repeated watchPosition calls
+    return new Promise(function(resolve, reject) {
+
+      // Start the timeout timer
+      startTimer(function() {
+
+        // On timeout
+
+        if(currentPosition === null) {
+
+          // We weren't able to get *any* position, so we return an
+          // error state
+          reject('Unable to retrieve position');
+
         }
+        else {
 
-      }).catch(function() {
+          // Return the best geolocation data we were able to get
+          navigator.geolocation.clearWatch(watchId);
+          resolve(currentPosition);
 
-        // Swallow the exception for this quick call
+        }
 
       });
-
-      // Set up the promise for the more accurate geolocation call using
-      // repeated watchPosition calls
-      return new Promise(function(resolve, reject) {
-
-        // Start measuring elapsed time
-        var time = 0;
-        var timeMeasureInterval = 10;
-
-        function updateTime()
-        {
-
-          time += timeMeasureInterval;
-
-          // We've exceeded our timeout
-          if(time > timeout) {
-
-            if(currentPosition === null) {
-
-              // We weren't able to get *any* position, so we return an
-              // error state
-              reject('Unable to retrieve position');
-
-            }
-            else {
-
-              // Return the best geolocation data we were able to get
-              clearInterval(intervalId);
-              navigator.geolocation.clearWatch(watchId);
-              resolve(currentPosition);
-
-            }
-
-          }
-
-        }
-
-        var intervalId = setInterval(updateTime, timeMeasureInterval);
 
         watchId = navigator.geolocation.watchPosition(
 
@@ -117,6 +110,7 @@ var geoid = (function() {
     });
   }
 
+  // Returns a quick, but inaccurate, geolocation result
   function getQuickPosition() {
 
     return new Promise(function(resolve, reject) {
@@ -142,6 +136,27 @@ var geoid = (function() {
       );
 
     });
+
+  }
+
+  function startTimer(callback) {
+
+    var time = 0;
+    var intervalId = 0;
+
+    function tick() {
+
+      time += timeMeasureInterval;
+
+      // We've exceeded our timeout
+      if(time > timeout) {
+        callback();
+        clearInterval(intervalId);
+      }
+
+    }
+
+    intervalId = setInterval(tick, timeMeasureInterval);
 
   }
 
